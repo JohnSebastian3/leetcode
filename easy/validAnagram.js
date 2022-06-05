b79778ac7d738dfd05c15afcef127e0302896418
John Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-06-05 12:12:17 -0400

    Add solved question for 6/5/2022

---
+++ easy/validAnagram.js
@@ -0,0 +1,47 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word 
+// or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+//---------- Solved on 6/5/2022 ----------
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) {
+    return false;
+  }
+
+    const firstLetters = s.split('');
+    const secondLetters = t.split('');
+    firstLetters.forEach(letter => {
+      if(secondLetters.includes(letter)) {
+        secondLetters.splice(secondLetters.indexOf(letter), 1);
+      }
+    })
+    return secondLetters.length === 0;
+};
+
+console.log(isAnagram('anagram', 'nagaram'));
+console.log(isAnagram('rat', 'car'));
+console.log(isAnagram('ab', 'a'));
+console.log(isAnagram('aacc', 'ccac'));
