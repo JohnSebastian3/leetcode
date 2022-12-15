a24792c65a8beb440b03f1e5d3bbcd8de7d2c9eb
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-15 17:55:30 -0500

    Add refactored solution that decreases time complexity from quadratic to linear

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -1,12 +1,10 @@
-// Given an array of integers nums and an integer target, return indices of 
+// Given an array of integers nums and an integer target, return indices of
 // the two numbers such that they add up to target.
 
-// You may assume that each input would have exactly one solution, and you 
+// You may assume that each input would have exactly one solution, and you
 // may not use the same element twice.
 
 // You can return the answer in any order.
-
- 
 
 // Example 1:
 
@@ -21,7 +19,6 @@
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
- 
 
 // Constraints:
 
@@ -35,37 +32,33 @@
 /**
  * @param {number[]} nums
  * @param {number} target
- * Input: an array of numbers and a target sum. We can assume there is only 
+ * Input: an array of numbers and a target sum. We can assume there is only
  * one solution
  * @return {number[]}
  * Output: array of indices of two numbers in the input array that satisfies
  * the target sum
- * 
+ *
  * @example:
  * twoSum([2,7,11,15], 18) == [1,2]
  */
- var twoSum = function(nums, target) {
-    const results = [];
-    // Loop through nums, each loop is checking the current number at the ith 
-    // index with the rest of the numbers in the array
-    for(let i = 0; i < nums.length; i++) {
-        let outerNumber = nums[i];
-        for(let j = i + 1; j < nums.length; j++) {
-            let currentSum = outerNumber + nums[j];
-            // if the ith number and the jth number create the target sum, push the value
-            // of i and j into a results array
-            if(currentSum == target) {
-                results.push(i);
-                results.push(j);
-                break;
-            }
-        }
+var twoSum = function (nums, target) {
+  const winningPairs = [];
+  const numbersObj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const differenceNeeded = target - nums[i];
+    if (numbersObj.hasOwnProperty(differenceNeeded)) {
+      winningPairs.push(nums.indexOf(differenceNeeded));
+      winningPairs.push(i);
     }
+    numbersObj[nums[i]] = differenceNeeded;
+  }
 
-    return results;
+  console.log(numbersObj);
 
-    // return the results array
+  return winningPairs;
 };
 
-// console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
+console.log(twoSum([2, 7, 11, 15], 18));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([0, 4, 3, 0], 0));
